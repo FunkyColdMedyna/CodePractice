639e24c5c01b83377fbc19d58fd97fdc91c8583d
Chris Medyna <chris.medyna@gmail.com>
2022-12-14 11:30:38 -0500

    updated merge sort attempt

--- ProblemSets/MergeSort1.js
+++ ProblemSets/MergeSort1.js
@@ -52,34 +52,93 @@ const data = [
 ] 
 
 //merge helper function for both 
-const merge = (left, right) => {
-    let sortedArr = [];
-    while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            sortedArr.push(left.shift());
+function merge (left, right) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            results.push(left[i]);
+            i++;
         } else {
-            sortedArr.push(right.shift());
+            results.push(right[j]);
+            j++;
         }
     } 
-    return [...sortedArr, ...left, ...right]
+    while ( i < left.length) {
+        results.push(left[i])
+        i++;
+    }
+    while (j < right.length) {
+        results.push(right[j]);
+        j++
+    }
+    return results;
 }
+
+console.log(merge([3],[5]));
+console.log(merge([5],[3]));
+
+
+//'regular' merge sort on an array 
+function mergeSort(arr){
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+} 
+
+console.log(mergeSort([10,69,2,12,33,99,1]));
+console.log(data[0].results) // payload : [ { profile : [object]}]
+console.log(data[0].results.payload[0]); // profile -> jenny
+console.log(data[0].results.payload[0].profile.rank); // 3
+console.log(data[0].results.payload[0].profile.favorites) //[ {top gun},  {mavericks}]
+console.log(data[0].results.payload[0].profile.favorites[0].rating) //7
+
+console.log(data[0].results.payload[1])
+
+let collection = [];
+
+
+
+//helper function to compare values? 
+// const compare  = ( a , b) => {
+
+// }
+
+// const mergeSortPayload = ( data ) => {
+//     //instantiate collection to perform merge sort on? 
+//     let collection = [];
+
+//     if ( profile.rank[payload1] < profile.rank[payload2]) {
+//         collection.push([payload1])
+//     } else { 
+//         collection.push([payload2])
+//     }
+//     //do something
+//     return collection;
+// }
+
+// mergeSortPayload(data);
 
 //ascending order -> rank only 
-const mergeSort = (data) => {
-    //data.results -> payloads ordered by (((data.results.payload.profile.rank)))
+// const mergeSort = (data) => {
+//     //data.results -> payloads ordered by (((data.results.payload.profile.rank)))
 
-    if (data.results.payload.length <= 1) return data.results.payload
-    let mid = Math.floor(data.results.payload.length / 2)
+//     // if (data.results.payload.length <= 1) return data.results.payload
+//     let mid = Math.floor(data.results.payload.length / 2)
 
-    let left = mergeSort(data.results.payload.slice(0, mid))
-    let right = mergeSort(data.results.payload.slice(mid))
+//     let left = mergeSort(data.results.payload.slice(0, mid))
+//     let right = mergeSort(data.results.payload.slice(mid))
 
-    return merge(left, right)
-}
+//     return merge(left, right)
+// }
 
-//ascending order --> rank parent (within each user object sorted by rating)
-const mergeSortRanked = (data) => {
+// //ascending order --> rank parent (within each user object sorted by rating)
+// const mergeSortRanked = (data) => {
 
-}
+// }
 
-console.log(mergeSort(data));
+// console.log(mergeSort(data));
